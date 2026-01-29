9d4168fa87234d6f172ac67d269462c649a5e04f
DuyHoang-06 <hoangdoan1105206@gmail.com>
2026-01-29 13:25:02 +0700

    Delete xacml-lab directory

--- xacml-lab/traversal.py
+++
@@ -1,6 +0,0 @@
-def generate_requests_from_paths(paths):
-    requests = []
-    for path in paths:
-        attr = extract_condition(path)
-        requests.append(build_request_xml(attr))
-    return requests
